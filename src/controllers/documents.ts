3d16ea0d9c22f3e4dd0271f90782b7696af335d8
lazaro molina <lazmolina@icloud.com>
2026-08-20 15:31:17 -0400

    Stpe 10 Chunk and Embed

--- src/controllers/documents.ts
+++ src/controllers/documents.ts
@@ -1,6 +1,13 @@
 import type { Request, Response } from 'express';
 
+import { readFileSync } from 'fs';
+import { PDFParse } from 'pdf-parse';
+
 import Document from '../models/document.js';
+import Chunk from '../models/chunk.js';
+
+import { chunkText } from '../utils/chunk.js';
+import { createEmbedding } from '../utils/embeddings.js';
 
 export const uploadDocument = async (
   req: Request,
@@ -10,18 +17,45 @@ export const uploadDocument = async (
     res.status(400).json({
       success: false,
       data: null,
-      error: { message: 'file is required' },
+      error: { message: 'File is required' },
     });
     return;
   }
 
-  const userId = req.user!.userId;
+  // Parse the PDF
+  const buffer = readFileSync(req.file.path);
 
-  const document = await Document.create({
-    title: req.file.originalname,
-    fileName: req.file.filename,
-    userId,
+  const parser = new PDFParse({
+    data: buffer,
   });
+
+  const { text } = await parser.getText();
+
+  // Split text into chunks
+  const chunks = chunkText(text);
+
+  // Use provided title or original filename
+  const title = req.body.title || req.file.originalname;
+
+  // Create document
+  const document = await Document.create({
+    title,
+    fileName: req.file.originalname,
+    userId: req.user!.userId,
+  });
+
+  // Create embeddings and save chunks
+  await Promise.all(
+    chunks.map(async (text) => {
+      const embedding = await createEmbedding(text);
+
+      return Chunk.create({
+        documentId: document._id,
+        text,
+        embedding,
+      });
+    }),
+  );
 
   res.status(201).json({
     success: true,
@@ -45,7 +79,10 @@ export const getDocuments = async (
   });
 };
 
-export const getDocument = (req: Request, res: Response): void => {
+export const getDocument = (
+  req: Request,
+  res: Response,
+): void => {
   res.status(200).json({
     success: true,
     data: {},
@@ -53,6 +90,9 @@ export const getDocument = (req: Request, res: Response): void => {
   });
 };
 
-export const deleteDocument = (req: Request, res: Response): void => {
+export const deleteDocument = (
+  req: Request,
+  res: Response,
+): void => {
   res.sendStatus(204);
 };
