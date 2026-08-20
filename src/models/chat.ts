355386f0004c572bdd093152b4b6c6e3a42430c1
lazaro molina <lazmolina@icloud.com>
2026-08-20 14:38:21 -0400

    Step 8 Chats

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -5,11 +5,13 @@ const chatSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
   },
+
   createdAt: {
     type: Date,
     default: Date.now,
